994dca09ee58a38fef7de332620dcd680aa2eab4
aalu1418 <aalu1418@gmail.com>
2020-02-04 21:23:18 -0500

    completed exercise to upload and retrieve from IPFS using react/js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import img from "./cat_bread_loaf.jpg";
+import "./App.css";
+
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient("http://localhost:5001");
 
 function App() {
+  const [imgHash, setImgHash] = React.useState();
+  const [img, setImg] = React.useState();
+
+  React.useEffect(() => {
+    const getIPFS = async () => {
+      const ver = await ipfs.version();
+      console.log("IPFS Version=", ver);
+    };
+    getIPFS();
+  }, []);
+
+  //load file to IPFS
+  const loadIPFS = async () => {
+    for await (const result of ipfs.add(img)) {
+    // const result = await ipfs.add("./cat_bread_loaf.jpg");
+    // console.log(result);
+    setImgHash(result.path);
+    }
+  };
+
+  //get image arraybuffer
+  const setImage = (event) => {
+    // const temp = event.target.files[0];
+    // console.log(temp);
+    setImg(event.target.files[0]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={setImage}/>
+        <button onClick={loadIPFS}>Load Image to IPFS</button>
+        {imgHash && <img src={`http://localhost:8080/ipfs/${imgHash}`} />}
       </header>
     </div>
   );
 }
+
 
 export default App;
